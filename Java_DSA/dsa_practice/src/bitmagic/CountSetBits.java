c76d11375d8d6d3da80a8c7687cfe3298d3e86a2
sarthu210 <sarthu102@gmail.com>
2026-07-08 22:29:25 +0530

    Count set bits done

--- Java_DSA/dsa_practice/src/bitmagic/CountSetBits.java
+++ Java_DSA/dsa_practice/src/bitmagic/CountSetBits.java
@@ -1,5 +1,29 @@
 package bitmagic;
 
 public class CountSetBits {
+	
+	public static int solution1(int n) {
+		int count = 0;
+		while(n>0) {
+			if(n%2==1) {
+				count = count + 1;
+			}
+			n = n/2;
+		}
+		return count;
+	}
+	
+	public static int solution2(int n) {
+		int count = 0;
+		while(n>0) {
+			n = n&(n-1);
+			count = count + 1;
+			
+		}
+		return count;
+	}
 
+	public static void main(String args[]) {
+		System.out.println(solution1(10));
+	}
 }
